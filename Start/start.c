c82c93f9dd076fc2c7a2d2cba1da7aa7b0197bcd
Mohamad Amer <mhdameraltesheh@gmail.com>
2024-09-29 19:29:02 +0300

    This is my First C Code

---
+++ Start/start.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //std : Standard. IO : Input & Output. .h : Header File
+
+/*int = Integer*/
+int main(){  // Main Function. You can write int main(void){} and it will also work 
+    printf("Hello World\n");  //printf: To Write
+    printf("I am Learning C\n"); // Use \n for a new Line - always finish statements with ; (semi colon)
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n*\t0\t#"); // \t tab (long Space)
+    return 0; //return 0 if the file worked, else, return 1
+}
